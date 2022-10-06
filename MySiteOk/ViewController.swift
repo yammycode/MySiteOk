598bd4f79850e27490874b98b1bd4996337c7ec2
Anton <info@yammycode.com>
2022-10-06 17:00:07 +0300

    Initial Commit

---
+++ MySiteOk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySiteOk
+//
+//  Created by Anton Saltykov on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
